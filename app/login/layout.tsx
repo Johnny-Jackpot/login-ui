701da62e365cc5cbf5ad186fe4a02ca517d78c63
oleksandrn <alexandrn@default-value.com>
2024-03-09 00:20:45 +0200

    Added logo and styles to login layout

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
-    <div>
-      <p>Logo goes here</p>
-      <div>{children}</div>
+    <div className='mt-8 sm:mt-[180px]'>
+      <Image
+        className='mx-auto'
+        src="/logo.svg"
+        alt="Qencode Logo"
+        width={178}
+        height={32}
+        priority
+      />
+      <div className='mt-10 sm:mt-[80px] mx-auto max-w-[400px] box-content px-2'>{children}</div>
     </div>
   );
 }
