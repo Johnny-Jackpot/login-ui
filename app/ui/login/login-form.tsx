fb47fc554be03c5a98a266a245c7e526717b8444
oleksandrn <alexandrn@default-value.com>
2024-03-09 19:00:57 +0200

    Added social button component

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -1,10 +1,11 @@
-'use client';
-
 import Button from "@/app/ui/button";
+import ButtonSocial from "@/app/ui/button-social";
 
 export default function LoginForm() {
   return (
     <form action="">
+      <ButtonSocial social='google' />
+      <ButtonSocial social='github' />
       <Button buttonType='primary'>Test</Button>
       <Button buttonType='secondary'>Test</Button>
     </form>
