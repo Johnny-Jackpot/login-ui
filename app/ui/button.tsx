4edbaed1e9de657f988bc7e75c0093ce3b506be4
oleksandrn <alexandrn@default-value.com>
2024-03-10 19:12:44 +0200

    Added login form component

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -3,22 +3,27 @@ import {clsx} from "clsx";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   buttonType?: 'primary' | 'secondary';
+  className?: string
 }
 
 export default function Button({
   children,
   buttonType = 'primary',
   type = 'button',
+  className,
   ...rest
 }: ButtonProps) {
-  const className = clsx(
-    `w-full h-12 rounded-lg flex justify-center items-center box-border text-base leading-[21px] font-medium`,
-    buttonType === 'primary' && ['bg-primary', 'text-white'],
-    buttonType === 'secondary' && ['border', 'border-secondary']
-  );
-
   return (
-    <button className={className} type={type} {...rest}>
+    <button
+      className={clsx(
+        `w-full h-12 rounded-lg flex justify-center items-center box-border`,
+        `text-base leading-[21px] font-medium ${className}`,
+        buttonType === 'primary' && ['bg-primary', 'text-white'],
+        buttonType === 'secondary' && ['border', 'border-secondary']
+      )}
+      type={type}
+      {...rest}
+    >
       {children}
     </button>
   );
--- app/ui/divider.tsx
+++ app/ui/divider.tsx
@@ -1,13 +1,16 @@
 export default function Divider({
-  children
+  children,
+  className,
 }: Readonly<{
   children: React.ReactNode;
+  className?: string,
 }>) {
   return (
     <div className={`
       before:h-px before:bg-[#E3E6E9] before:grow before:mr-[5px]
       h-4 flex items-center uppercase text-xs font-medium text-[#BEC5CC]
       after:h-px after:bg-[#E3E6E9] after:grow after:ml-[5px]
+      ${className}
     `}>{children}</div>
   );
 }
