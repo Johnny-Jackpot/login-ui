b689b72df4e50c60f4536ae5a3c20a0984000bab
oleksandrn <alexandrn@default-value.com>
2024-03-10 18:13:45 +0200

    Added input component

--- app/ui/inputs/input.tsx
+++ app/ui/inputs/input.tsx
@@ -1,11 +1,28 @@
-export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
-  const {type = 'text', ...rest} = props;
+import {clsx} from "clsx";
 
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  iconBefore?: React.ReactNode;
+  iconAfter?: React.ReactNode;
+}
+
+export default function Input({
+  type = 'text',
+  iconBefore,
+  iconAfter,
+  ...rest
+}: InputProps) {
   return (
-    <input
-      className='w-full rounded-lg h-12 box-border border border-secondary outline-red outline-primary text-[15px] leading-5 p-3.5 '
-      type={type}
-      {...rest}
-    />
+    <div className='rounded-lg h-12 box-border border border-secondary flex items-center px-3.5'>
+      {iconBefore}
+      <input
+        className={clsx('w-full text-[15px] leading-5 outline-none', {
+          'ml-3.5': iconBefore,
+          'mr-3.5': iconAfter
+        })}
+        type={type}
+        {...rest}
+      />
+      {iconAfter}
+    </div>
   );
 }
