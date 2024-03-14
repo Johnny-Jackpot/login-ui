e25c05c1814efbf932d6ddd622cd4bddc30b8d10
oleksandrn <alexandrn@default-value.com>
2024-03-15 00:52:41 +0200

    Added mocks

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Home page after login goes here</p>
+      <p>Clear cookies to logout</p>
     </main>
   );
 }
