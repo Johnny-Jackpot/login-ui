81eac3ab5efc570fe92c2d98a064db832fdf9285
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-03-15 00:59:10 +0200

    Added redirect to login form

--- app/ui/login/forgot-password-form.tsx
+++ app/ui/login/forgot-password-form.tsx
@@ -1,12 +1,17 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
 import Input from "@/app/ui/inputs/input";
 import Button from "@/app/ui/button";
 
 export default function ForgotPasswordForm() {
+  const router = useRouter();
+
   return (
     <form action="">
       <Input placeholder='Enter your email' wrapperClassName='mb-[25px]' />
       <Button type='submit' className='mb-5'>Send</Button>
-      <Button buttonType='secondary'>Cancel</Button>
+      <Button buttonType='secondary' onClick={() => router.push('/login')}>Cancel</Button>
     </form>
   );
 }
